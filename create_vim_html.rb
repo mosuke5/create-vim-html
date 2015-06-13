0901cf7adb9665baaa393ee2039faa23bdc81ea7
mosuke5 <g.vamos603@gmail.com>
2015-06-13 15:42:08 +0900

    update

--- create_vim_html.rb
+++ create_vim_html.rb
@@ -1,15 +1,17 @@
-config_file = ARGV[0]
-source_file = ARGV[1]
+color_schemes_file = ARGV[0]
+source_files       = ARGV[1]
 
-abort "error1" if config_file.nil?
-abort "error2" if source_file.nil?
+abort "error1" if color_schemes_file.nil?
+abort "error2" if source_files.nil?
 
-vim_options = ""
-File.open(config_file) do |file|
-  file.each_line do |conf|
-    next if conf.chomp.empty?
-    vim_options += "+\"#{conf.chomp}\""
+File.open(color_schemes_file) do |colors|
+  colors.each_line do |color|
+    vim_options = "+\"syntax on\"+\"set number\"+\"colorscheme #{color.chomp}\""
+    File.open(source_files) do |files|
+      files.each_line do |file|
+        #puts("vim -f #{vim_options} +\"TOhtml\" +\"wq #{file.chomp}.#{color.chomp}.html\" +\"q\" #{file.chomp}")
+        system("vim -f #{vim_options} +\"TOhtml\" +\"wq! #{file.chomp}.#{color.chomp}.html\" +\"q\" #{file.chomp}")
+      end
+    end
   end
 end
-
-system("vim -f #{vim_options} +\"TOhtml\" +\"wq\" +\"q\" #{source_file}")
--- sample.rb
+++ sample.rb
@@ -1,16 +1,22 @@
-require 'logger'
-require 'docker'
-require 'optparse'
+# The Hello class
+class HelloWorld
+  Version = "1.0"
 
-args = {:expire_sec => '7200',
-        :vimrc_path => nil,
-        :docker_host => 'localhost', 
-        :docker_port => '4243',}
+  def initialize(myname="Ruby")
+    @name = myname
+  end
 
-OptionParser.new do |opt|
-  opt.on('-s=SEC', '--expire-sec', 'default: 7200') { |v| args[:expire_sec] = v }
-  opt.on('-p=PATH', '--vimrc-path', 'もし指定しなければvimrcは消しません') { |v| args[:vimrc_path] = v }
-  opt.on('-H=HOST', '--docker-host', 'default: localhost') { |v| args[:docker_host] = v }
-  opt.on('-P=PORT', '--docker-port', 'default: 4243') { |v| args[:docker_port] = v }
-  opt.parse!(ARGV)
+  def hello
+    print "Hello, world. I am ", @name, ".\n"
+  end
 end
+
+# Create new objects
+bob   = HelloWorld.new("Bob")
+alice = HelloWorld.new("Alice")
+ruby  = HelloWorld.new
+
+# Output
+bob.hello
+alice.hello
+ruby.hello
